e3aa31cc851ed6b532a3d0af6f8831d18f4fee8d
scarfinor <scarfinor@gmail.com>
2024-10-14 21:45:59 -0500

    added UserController.java add.html User.java

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,16 +1,26 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
-    @GetMapping("add")
-    public String displayAddUserForm() {
+    @RequestMapping("add")
+    public String displayAddUserForm(Model model) {
+        return "user/add";
+    }
 
-        return "add";
+    @PostMapping("")
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        if (!user.getPassword().equals(verify)) {
+            model.addAttribute("username", user.getUsername());
+            model.addAttribute("email", user.getEmail());
+            return "user/add";
+        }
+        return "'user/index";
     }
 }
